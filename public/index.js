a300ed858933ed5de10858e73f5bd4b307e8858e
Nicholas Walton <ngwalton@gmail.com>
2024-03-01 21:54:54 -0600

    Prevent default on hamburger button

--- public/index.js
+++ public/index.js
@@ -123,7 +123,8 @@ function updateVisibleSearchField() {
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 
-toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', (event) => {
+  event.preventDefault();
   navbarLinks.classList.toggle('active');
 });
 
